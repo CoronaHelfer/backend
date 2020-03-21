077598476feed5fad1d9fb7bcf741b29bb73660c
Tom Böhling <tom-boehling@gmx.de>
2020-03-21 20:10:46 +0100

    distanceBetweenTwoCoordinates

--- src/app/geocoding/GeocodingService.ts
+++ src/app/geocoding/GeocodingService.ts
@@ -29,8 +29,15 @@ class GeocodingService {
     }
 
     public static distanceBetweenTwoCoordinates(lat1, lon1, lat2, lon2): number {
+        var _eQuatorialEarthRadius = 6378.1370;
+        var _d2r = (Math.PI / 180.0);
+        var dlong = (lon2 - lon1) * _d2r;
+        var dlat = (lat2 - lat1) * _d2r;
+        var a = Math.pow(Math.sin(dlat / 2.0), 2.0) + Math.cos(lat1 * _d2r) * Math.cos(lat2 * _d2r) * Math.pow(Math.sin(dlong / 2.0), 2.0);
+        var c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1.0 - a));
+        var d = _eQuatorialEarthRadius * c;
 
-        return 0;
+        return d; //in km
     }
 }
 
