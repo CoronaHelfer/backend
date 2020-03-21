b8ea1275acd8a6716fbd05602ab54cf28169d444
Janneck Lange <info@jannecklange.de>
2020-03-21 13:43:09 +0100

    add routes to Categorys

--- src/config/route/routes.ts
+++ src/config/route/routes.ts
@@ -1,6 +1,7 @@
 
 import AuthRoute from '../../app/auth/AuthRoute';
 import UserRoute from '../../app/auth/UserRoute';
+import CategoryRoute from '../../app/helpRequests/category/CategoryRoute';
 
 const Routes = [
   {
@@ -9,10 +10,14 @@ const Routes = [
   },
   {
     guard: false,
+    route: CategoryRoute,
+    url: 'category',
+  },
+  {
+    guard: false,
     route: AuthRoute,
     url: 'auth',
   },
-
 ];
 
 export default Routes;
