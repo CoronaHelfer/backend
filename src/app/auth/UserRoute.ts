3fa648a5dacd8ac1496381d64e29fe396fa8c804
Janneck Lange <info@jannecklange.de>
2020-03-21 18:36:07 +0100

    fix user delete

--- src/app/auth/UserRoute.ts
+++ src/app/auth/UserRoute.ts
@@ -4,5 +4,6 @@ import UserController from './UserController';
 const router = Router();
 
 router.get('/', UserController.search);
+router.delete('/', UserController.delete);
 
 export default router;
