2150baa19c521093c64b27ecdcfca1d7a950833e
Janneck Lange <info@jannecklange.de>
2020-03-21 03:30:48 +0100

    change package name to match pattern

--- src/app/auth/UserController.ts
+++ src/app/auth/UserController.ts
@@ -2,33 +2,47 @@ import UserService from './UserService';
 
 class UserController {
 
-  public login(req, res) {
-    const body = req.body;
-    UserService.isValidUser(body)
-      .then((result) => res.status(200).send({ token: result }))
-      .catch((err) => res.status(500).send({ error: err.message }));
-  }
+    public login(req, res) {
+        const body = req.body;
+        UserService.isValidUser(body)
+            .then((result) => res.status(200).send({token: result}))
+            .catch((err) => res.status(500).send({error: err.message}));
+    }
 
-  public register(req, res) {
-    const body = req.body;
-    UserService.create(body)
-      .then((result) =>  res.status(201).send({ token: result }))
-      .catch((err) => res.status(500).send({ error: err.message }));
-  }
+    public register(req, res) {
+        const body = req.body;
+        UserService.create(body)
+            .then((result) => res.status(201).send({token: result}))
+            .catch((err) => res.status(500).send({error: err.message}));
+    }
 
-  public search(req, res) {
-    UserService.find({})
-      .then((result) => res.status(200).send({ users: result }))
-      .catch((err) => res.status(500).send({ error: err.message }));
-  }
+    public search(req, res) {
+        UserService.find({})
+            .then((result) => res.status(200).send({users: result}))
+            .catch((err) => res.status(500).send({error: err.message}));
+    }
 
-  public update() {
-    // Update Code would written here
-  }
+    public didExist(id: string): Promise<boolean> {
+        return new Promise((res, rej) => {
+            UserService.find({_id: id})
+                .then((result) => {
+                    if (result.length > 0) {
+                        res(true);
+                    } else {
+                        res(false);
+                    }
+                })
+                .catch((err) => res(false));
+        });
+    }
 
-  public delete() {
-    // delete Code would written here
-  }
+    public update() {
+        // Update Code would written here
+    }
+
+    public delete() {
+        // delete Code would written here
+    }
 }
 
 export default new UserController();
