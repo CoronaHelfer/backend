2b13de02cd03ce6ef3999d11a964c2edbd7d9706
Janneck Lange <info@jannecklange.de>
2020-03-21 11:04:14 +0100

    add external mongodb server config

--- src/config/environments/config.ts
+++ src/config/environments/config.ts
@@ -10,7 +10,7 @@ class Config {
     // this.configENV = process.env;
     this.PORT = '3000';
     this.API_ROOT_PATH = 'api';
-    this.DB_URL = 'mongodb://127.0.0.1:27017/corona-helper';
+    this.DB_URL = 'mongodb://corona-helper:wirvsvirus2020@134.255.225.148:27072/admin?authSource=admin&readPreference=primary';
     this.JWT_TOKEN_SECRET = '';
     this.JWT_EXPIRE_TIME = 86400; // 1 week
   }
