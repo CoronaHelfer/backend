0f5e2b2b39768fce8aff4d02a35fd77a39cb1f6e
Tom Böhling <tom-boehling@gmx.de>
2020-03-21 21:33:48 +0100

    distanceBetweenTwoCoordinates working addressToCoordinate not working

--- src/app/geocoding/GeocodingService.ts
+++ src/app/geocoding/GeocodingService.ts
@@ -1,4 +1,4 @@
-import config from '../../config/environments'; //please config there
+import Environment from '../../config/environments'; //please config there
 import { Client } from "@googlemaps/google-maps-services-js";
 
 const client = new Client({});
@@ -9,12 +9,12 @@ class GeocodingService {
         client.geocode({
             params: {
                 address: street + streetNr + plz + city,
-                key: config.googleApiKey
+                key: Environment.googleApiKey
             },
             timeout: 1000 // milliseconds
         })
             .then(r => {
-                return {
+                return { //not working
                     lat: r.data.results[0].geometry.location.lat,
                     lon: r.data.results[0].geometry.location.lng,
                 };
@@ -22,7 +22,7 @@ class GeocodingService {
             .catch(e => {
                 console.log(e);
             });
-        return {
+        return { //not working
             lat: 0,
             lon: 0
         }
@@ -42,3 +42,6 @@ class GeocodingService {
 }
 
 export default new GeocodingService();
+
+//test
+//console.log(GeocodingService.addressToCoordinate(27404, "Stader Str.", "Heeslingen", "3"))
