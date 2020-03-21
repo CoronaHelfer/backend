72b4b4673155c6045b54a70db8224b3ee808142c
Janneck Lange <info@jannecklange.de>
2020-03-21 13:42:42 +0100

    change mongo collection

--- src/config/environments/config.ts
+++ src/config/environments/config.ts
@@ -10,11 +10,10 @@ class Config {
     // this.configENV = process.env;
     this.PORT = '3000';
     this.API_ROOT_PATH = 'api';
-    this.DB_URL = 'mongodb://corona-helper:wirvsvirus2020@134.255.225.148:27072/admin?authSource=admin&readPreference=primary';
+    this.DB_URL = 'mongodb://corona-helper:wirvsvirus2020@134.255.225.148:27072/coronaHelper?authSource=admin&readPreference=primary';
     this.JWT_TOKEN_SECRET = '';
     this.JWT_EXPIRE_TIME = 86400; // 1 week
   }
-
 }
 
 export default Config;
