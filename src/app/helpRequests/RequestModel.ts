b6ddbc9b1d2b45f0ba22bfee6b6c1ddca33f96af
Janneck Lange <info@jannecklange.de>
2020-03-21 12:02:01 +0100

    add RequestModel

--- src/app/helpRequests/RequestModel.ts
+++ src/app/helpRequests/RequestModel.ts
@@ -1,12 +1,76 @@
-import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import { isEmail } from 'validator';
-import { RequestMiddleware } from './RequestMiddleware';
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+import {RequestMiddleware} from './RequestMiddleware';
 
 class Request extends mongoose.Schema {
     public request: mongoose.Schema;
+
     constructor() {
         const RequestSchema = {
+            title: {
+                type: String,
+                required: true,
+            },
+            description: {
+                type: String,
+                required: true,
+            },
+            category: {
+                type: ObjectId,
+                ref: 'Category',
+                required: true,
+            },
+            created_by: {
+                type: ObjectId,
+                ref: 'User',
+                required: true,
+            },
+            address: {
+                position: {
+                    latitude: {
+                        type: String,
+                        required: true,
+                    },
+                    longitude: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                plz: {
+                    type: String,
+                    required: true,
+                },
+                city: {
+                    type: String,
+                    required: true,
+                },
+                street: {
+                    type: String,
+                    required: true,
+                },
+                street_nr: {
+                    type: String,
+                    required: true,
+                },
+            },
+            time_start: {
+                type: Date,
+                required: true,
+            },
+            time_end: {
+                type: Date,
+                required: false,
+            },
+            helper: [{
+                helperId: {
+                    type: ObjectId,
+                    ref: 'User',
+                },
+                offer_text: {
+                    type: String,
+                },
+            }],
         };
         const request = super(RequestSchema, {
             timestamps: {
@@ -20,4 +84,5 @@ class Request extends mongoose.Schema {
         return this.request;
     }
 }
+
 export default mongoose.model('Request', new Request());
