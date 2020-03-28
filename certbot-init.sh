52d7b8d2a8c931c82a42fcf1aa3a423cb4af63a5
Markus <marb.dev@outlook.com>
2020-03-28 22:10:00 +0100

    Fix the domain for which the server certificate is created

--- certbot-init.sh
+++ certbot-init.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(dev.coronahelfer.eu)
+domains=(coronahelfer.eu)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="info@coronahelfer.eu" # Adding a valid address is strongly recommended
