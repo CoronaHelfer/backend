27e84e7d930a45a0a4caa49c855d05e10adc529e
Janneck Lange <info@jannecklange.de>
2020-04-01 00:52:23 +0200

    change db to ssl replica cluster

--- src/config/db/connection.ts
+++ src/config/db/connection.ts
@@ -1,20 +1,30 @@
+import fs from 'fs';
 import mongoose from 'mongoose';
 import Environment from '../environments';
 
 const config = Environment;
 const dbConectionString = config.DB_URL;
-class DBConection {
-  constructor() {
-    mongoose.connect(dbConectionString, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  });
-  }
+const certPath = config.CERT_PATH;
 
-  public errorHandler() {
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {
-      console.info(`${dbConectionString} DB is Connected with this App`);
-    });
-  }
+class DBConection {
+    constructor() {
+        const key = fs.readFileSync(certPath);
+
+        mongoose.connect(dbConectionString, {
+            sslCA: key,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+    }
+
+    public errorHandler() {
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', () => {
+            console.info(`${dbConectionString} DB is Connected with this App`);
+        });
+    }
 }
 
 export = DBConection;
--- src/config/environments/config.ts
+++ src/config/environments/config.ts
@@ -8,13 +8,15 @@ class Config {
     private JWT_EXPIRE_TIME: number;
     private DEFAULT_PROFILE_PICTURES: string[];
     private REQUEST_MAX_DISTANCE: number;
+    private CERT_PATH: string;
 
     constructor() {
         // this.configENV = process.env;
         this.PORT = '3000';
         this.API_ROOT_PATH = 'api';
-        this.DB_URL = 'mongodb://corona-helper:wirvsvirus2020@134.255.225.148:27072/coronaHelper?authSource=admin&readPreference=primary';
-        this.JWT_TOKEN_SECRET = '';
+        this.DB_URL = '!!!DO NOT COMMIT THIS LINE!!!';
+        this.CERT_PATH = '!!!DO NOT COMMIT THIS LINE!!!';
+        this.JWT_TOKEN_SECRET = '!!!DO NOT COMMIT THIS LINE!!!';
         this.JWT_EXPIRE_TIME = 86400; // 1 week
         this.googleApiKey = '!!!DO NOT COMMIT THIS LINE!!!';
         this.DEFAULT_PROFILE_PICTURES = [
