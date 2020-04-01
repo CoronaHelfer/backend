ff2fb2b5fdc8b7d8f02e0da3924394e9e7818f13
Janneck Lange <info@jannecklange.de>
2020-04-02 01:21:15 +0200

    remove db username/password/collection from config (set in prod or dev config)

--- src/config/environments/config.ts
+++ src/config/environments/config.ts
@@ -17,9 +17,6 @@ class Config {
         // this.configENV = process.env;
         this.PORT = '3000';
         this.API_ROOT_PATH = 'api';
-        this.DB_USERNAME = '!!!DO NOT COMMIT THIS LINE!!!';
-        this.DB_PASSWORD = '!!!DO NOT COMMIT THIS LINE!!!';
-        this.DB_COLLECTION = 'coronahelfer-dev';
         this.DB_PORT = 32470;
         this.DB_URL = 'ce9f6d8d-e3b1-4c04-9481-fbef09eb3941-0.bpb68u2f0gvaqgd0n64g.databases.appdomain.cloud';
         this.CERT_PATH = __dirname.substring(0, __dirname.length - 30) + 'src\\config\\db\\cert';
