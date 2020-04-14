0d40e913923379a6a357a6b8e5dd3800684ecc55
Janneck Lange <info@jannecklange.de>
2020-04-14 20:33:16 +0200

    change connection environment - BINDING is now a alternative

--- src/config/db/connection.ts
+++ src/config/db/connection.ts
@@ -10,20 +10,20 @@ if (process.env.BINDING) {
   binding = JSON.parse(process.env.BINDING);
 }
 
-if (binding === undefined) {
+if (!((binding) || (process.env.DB_USERNAME && process.env.DB_PASSWORD && process.env.DB_HOSTNAME &&
+  process.env.DB_PORT && process.env.DB_CERT))) {
   console.error('ENVIRONMENT variable "BINDING" is not set! MongoDB must be bound to IBM Kubernetes Cluster.');
   process.exit(1);
 }
 
-const mongodb = binding.connection.mongodb;
-const authentication = mongodb.authentication;
+const username = process.env.DB_USERNAME || binding.connection.mongodb.authentication.username;
+const password = process.env.DB_PASSWORD || binding.connection.mongodb.authentication.password;
+const connectionString = `mongodb://${username}:${password}@${process.env.DB_HOSTNAME || binding.connection.mongodb.hosts[0].hostname}:${process.env.DB_PORT || binding.connection.mongodb.hosts[0].port},host-2:32470/${config.DB_COLLECTION}?authSource=admin&replicaSet=replset&ssl=true`;
+console.log(connectionString);
 
-const username = authentication.username;
-const password = authentication.password;
-const connectionPath = mongodb.hosts;
-const connectionString = `mongodb://${username}:${password}@${connectionPath[0].hostname}:${connectionPath[0].port},${connectionPath[1].hostname}:${connectionPath[1].port}/${config.DB_COLLECTION}?authSource=admin&replicaSet=replset&ssl=true`;
+const ca = process.env.DB_CERT ? [Buffer.from(process.env.DB_CERT, 'base64')] :
+  [Buffer.from(binding.connection.mongodb.certificate.certificate_base64, 'base64')];
 
-const ca = [Buffer.from(mongodb.certificate.certificate_base64, 'base64')];
 const options = {
   ssl: true,
   sslValidate: true,
