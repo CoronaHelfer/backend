2b835b14bce480eccbaab160a96c6ee79a119945
Kevin Ley <3976145+kevinley@users.noreply.github.com>
2020-05-10 13:20:16 +0200

    chore: more refactoring

--- src/resources/request/request-controller.ts
+++ src/resources/request/request-controller.ts
@@ -4,12 +4,26 @@ import sendResponse from '../../utils/send-response';
 import handleError from '../../utils/handle-error';
 import Response from '../../utils/response';
 
+import { mergeDeepRight } from 'ramda';
+import { createQuery, createProjection, createListOptions } from '../../utils/mongoose-helpers';
+
 const requestController = createController({ Model: RequestModel }) as any;
 
 requestController.getOwn = sendResponse(handleError(async (req, res) => {
-  const requests = await RequestModel.find({ created_by: req.decoded._id });
+  const query = createQuery(req.query);
+  const projection = createProjection(req.query);
+  const options = createListOptions(req.query);
 
-  return Response.ok(requests);
+  const total = await RequestModel.countDocuments();
+
+  const ownRequestsQuery = mergeDeepRight(query, { created_by: req.decoded._id });
+  const entities = await RequestModel.find(ownRequestsQuery, projection, options);
+
+  return Response.ok({
+    total,
+    count: entities.length,
+    data: entities,
+  });
 }));
 
 export default requestController;
--- src/resources_deprecated/helper/HelperService.ts
+++ src/resources_deprecated/helper/HelperService.ts
@@ -1,5 +1,5 @@
 import User from '../auth/UserModel';
-import Request from '../helpRequests/RequestModel';
+import Request from '../../resources/request/request-model';
 import NotificationService from '../notification/NotificationService';
 
 class HelperService {
